0f874b3d846a106aab3133e57c40594531cda233
jongrover <hello@jonathangrover.com>
2015-08-20 20:08:56 -0400

    finish for now

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+$(function(){
+
+  var numOfSlides = $('.slide').size(),
+      slideWidth = $('.slide').width(),
+      slidePos = 0,
+      speed = 600;
+
+  $('.tray').width(numOfSlides * slideWidth);
+
+  $('.next').click(function(event){
+    event.preventDefault();
+    if (slidePos < numOfSlides - 1) {
+      slidePos += 1;
+    } else {
+      slidePos = 0;
+    }
+    var distance = slidePos * slideWidth;
+    $(this).parent().find('.tray').animate({right: distance}, speed);
+  });
+
+  $('.prev').click(function(event){
+    event.preventDefault();
+    if (slidePos > 0) {
+      slidePos -= 1;
+    } else {
+      slidePos = numOfSlides - 1;
+    }
+    var distance = slidePos * slideWidth;
+    $(this).parent().find('.tray').animate({right: distance}, speed);
+  });
+});
